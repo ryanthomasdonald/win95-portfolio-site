b1bc6f83dbdc2e5d529ac6f01f9b5edb51379584
Ryan Donald <ryanthomasdonald@gmail.com>
2023-04-18 00:02:16 -0400

    resume update

--- client/src/pages/LetsChat.tsx
+++ client/src/pages/LetsChat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, MouseEvent } from "react";
+import React, { useState } from "react";
 import { Rnd } from "react-rnd";
 import { useAppDispatch } from '../app/hooks';
 import { updateShowChat } from '../features/window/windowSlice';
@@ -41,23 +41,23 @@ function LetsChat(): JSX.Element {
         <div className="window-content">
             <div className="row title-bar-bg-blue gx-0">
             <div className="col-11 draggable">
-                <img className="title-bar-left" src="img/lets-chat-title-bar-left.png" />
+                <img className="title-bar-left" alt="title bar" src="img/lets-chat-title-bar-left.png" />
             </div>
             <div className="col-1 d-flex justify-content-end">
-                <img className="title-bar-right" src="img/window-header-title-bar-right.png" useMap="#closeChat" />
+                <img className="title-bar-right" alt="title bar" src="img/window-header-title-bar-right.png" useMap="#closeChat" />
                 <map name="closeChat">
-                    <area onClick={(e) => handleCloseChat(e)} shape="rect" coords="68,2,100,32" href="#" />
+                    <area onClick={(e) => handleCloseChat(e)} alt="close window" shape="rect" coords="68,2,100,32" href="#" />
                 </map>
             </div>
             </div>
             <div className="row menu-bar-bg-gray gx-0">
             <div className="col-6 menu-bar-bg-gray">
-                <img className="menu-bar-left" src="img/window-header-menu-bar-left.png" />
+                <img className="menu-bar-left" alt="menu bar" src="img/window-header-menu-bar-left.png" />
             </div>
             </div>
             <div className="window-body-border">
             <p className="p-without-img-above">
-                I'm really excited to begin this new chapter in my life and if I've learned ANYTHING from my years 
+                I'm excited to continue this new chapter in my life and if I've learned ANYTHING from my years 
                 as a professional musician, it's that having a healthy network is a must when searching for 
                 new opportunities.
                 <br />
@@ -66,18 +66,18 @@ function LetsChat(): JSX.Element {
                 <br />
                 <div className="row gx-0">
                     <div className="col-6 d-flex justify-content-end px-3 py-4">
-                        <a href="https://github.com/ryanthomasdonald" target="_blank"><img src="img/github-logo.png" alt="GitHub" width="130px" /></a>
+                        <a href="https://github.com/ryanthomasdonald" target="blank"><img src="img/github-logo.png" alt="GitHub" width="130px" /></a>
                     </div>
                     <div className="col-6 d-flex justify-content-start px-3 py-4">
-                        <a href="https://www.linkedin.com/in/ryan-donald-96351221a/" target="_blank"><img src="img/li-logo.png" alt="LinkedIn" width="130px" /></a>
+                        <a href="https://www.linkedin.com/in/ryan-donald-96351221a/" target="blank"><img src="img/li-logo.png" alt="LinkedIn" width="130px" /></a>
                     </div>
                 </div>
                 <div className="row gx-0">
                     <div className="col-6 d-flex justify-content-end px-3">
-                        <a href="mailto:ryanthomasdonald@gmail.com" target="_blank"><img src="img/gmail-logo.png" alt="Gmail" width="130px" /></a>
+                        <a href="mailto:ryanthomasdonald@gmail.com" target="blank"><img src="img/gmail-logo.png" alt="Gmail" width="130px" /></a>
                     </div>
                     <div className="col-6 d-flex justify-content-start px-4">
-                        <a className="resume-align" href="files/ryan-donald-resume-2022.pdf" target="_blank"><img width="104px" src={resumeImg} onMouseOver={() => ShowResumeLetters()} onMouseOut={() => HideResumeLetters()} alt="resume" /></a>
+                        <a className="resume-align" href="files/ryan-thomas-donald-resume-2023.pdf" target="blank"><img width="104px" src={resumeImg} onMouseOver={() => ShowResumeLetters()} onMouseOut={() => HideResumeLetters()} alt="resume" /></a>
                     </div>
                 </div>
             </p>
--- client/src/pages/QuakeReadme.tsx
+++ client/src/pages/QuakeReadme.tsx
@@ -33,18 +33,18 @@ function QuakeReadme(): JSX.Element {
             <div className="window-content">
                 <div className="row title-bar-bg-blue gx-0">
                 <div className="col-11">
-                    <img className="title-bar-left draggable" src="img/quake-readme-title-bar-left.png" />
+                    <img className="title-bar-left draggable" alt="title bar" src="img/quake-readme-title-bar-left.png" />
                 </div>
                 <div className="col-1 d-flex justify-content-end">
-                    <img className="title-bar-right" src="img/window-header-title-bar-right.png" useMap="#closeQuakeReadme" />
+                    <img className="title-bar-right" alt="title bar" src="img/window-header-title-bar-right.png" useMap="#closeQuakeReadme" />
                     <map name="closeQuakeReadme">
-                        <area onClick={(e) => handleCloseQuakeReadme(e)} shape="rect" coords="68,2,100,32" href="#" />
+                        <area onClick={(e) => handleCloseQuakeReadme(e)} alt="close window" shape="rect" coords="68,2,100,32" href="#" />
                     </map>
                 </div>
                 </div>
                 <div className="row menu-bar-bg-gray gx-0">
                 <div className="col-6 menu-bar-bg-gray">
-                    <img className="menu-bar-left" src="img/window-header-menu-bar-left.png" />
+                    <img className="menu-bar-left" alt="menu bar" src="img/window-header-menu-bar-left.png" />
                 </div>
                 </div>
                 <div className="window-body-border">
